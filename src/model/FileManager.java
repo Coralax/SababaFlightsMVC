69260d2f55f5d2f1f35c8402cc3ea40c11393da9
Coral <coral.ifergan@gmail.com>
2020-06-14 11:48:14 +0300

    1.Minor changes before merge to master

--- src/model/FileManager.java
+++ src/model/FileManager.java
@@ -1,6 +1,21 @@
 package model;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.Set;
 
-public class FileManager<T extends Serializable > {
+public class FileManager<T> {
+
+    private String fileName;
+    public FileManager(String fileName) {
+        this.fileName = fileName;
+    }
+    public void write(Set<T> object){
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(this.fileName))) {
+            objectOutputStream.writeObject(object);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Cannot write to the file");
+        }
+   }
 }
